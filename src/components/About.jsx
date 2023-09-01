9563aae60a99e2d62dc0792d9710201e10356806
DilanGerber <130708466+DilanGerber@users.noreply.github.com>
2023-08-31 19:23:19 -0500

    Ultimo commit antes del CP

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 const About = () => {
     return (
             <div>
-                <h1> Hola Dilan </h1>
+                <h1> Hola Henry </h1>
+                <p> Perdon pero los estilos para otro día </p>
             </div>
         )
     }
